4fa2bb469aaadecf32116861523086ad2f4a37d0
harlamovads <90269992+harlamovads@users.noreply.github.com>
2022-05-07 20:13:34 +0300

    0.4. -> 0.5.
    1. Появилась возможность бить текст на кусочки по разделителю пользователя и смотреть топ-3 частотности имен собственных в каждом кусочке. 
    2. Код чуть приведен в порядок
    3. Теперь разные режимы работают согласованно

--- Nameanalyzer.py
+++ Nameanalyzer.py
@@ -1,14 +1,8 @@
 import time
 from tqdm import tqdm
 import re
-#from enchant.checker import SpellChecker
 import spacy
 from itertools import combinations
-
-#def spelchek(strochechka): #спеллчекер (не нужен, но мне жалко удалять, он хороший)
-#    checker = SpellChecker("en_US")
-#    checker.set_text(strochechka)
-#    return([i.word for i in checker])
 
 def normnamefinder(strocka): #(спейситыпросто)космос
     doc = nlp(strocka)
@@ -22,95 +16,140 @@ def normnamefinder(strocka): #(спейситыпросто)космос
     return(spisok)
 
 filename = input("Введите, пожалуйста, имя файла с расширением ")
-abzinfo = input("Чем разделены абзацы в документе? Если двумя отступами - введите 1, одним отступом - нажмите 2, табуляцией - нажмите 3 ")
-searchmode = input("Вы хотите провести поиск по конкретным именам персонажей? Если да, нажмите 1, если нет - нажмите 2 ")
+searchmode = input("Что вы хотите сделать? Если собирать граф по конкретным именам персонажей, нажмите 1, если хотите сначала найти все имена - нажмите 2, если хотите посмотреть самые частотные имена по главам - нажмите 3 ")
+if searchmode == "3":
+    utoch = input("Если Вы хотите использовать для разделения слово, нажмите 1, если количество пустых строк - нажмите 2 ")
+    if utoch == "1":
+        abzinfo = input("Введите без кавычек слово, которое Вы хотите использовать для разделения текста. ")
+if searchmode == "1" or searchmode == "2":
+    utoch = input("Чем разделены абзацы в документе? Если табуляцией - нажмите 1, если пустыми строками - нажмите 2 ")
+    if utoch == "1":
+        abzinfo = "\t"
+if utoch == "2":
+        perem = int(input("Сколько пустых строк будут отделять новый кусок текста? "))
+        abzinfo = "\n" * perem
+
 if searchmode == "1":
-    worklist_us = input("Введите через пробел все имена персонажей, которые Вас интересуют ")
-    worklist_us = worklist_us.split(" ")
-    changebiglist_us = []
-    for wds in worklist_us:
-        if "_" in wds:
-            worklist_us.remove(wds)
-            changelist_us = wds.split("_")
-            changebiglist_us.append(changelist_us)
-            for thns in changelist_us:
-                worklist_us.append(thns)
-            changelist_us = []
+    worklist_1 = input("Введите через пробел все имена персонажей, которые Вас интересуют ")
+    worklist_1 = worklist_1.split(" ")
+    changebiglist_1 = []
+    for wds_1 in worklist_1:
+        if "_" in wds_1:
+            worklist_1.remove(wds_1)
+            changelist_1 = wds_1.split("_")
+            changebiglist_1.append(changelist_1)
+            for things_1 in changelist_1:
+                worklist_1.append(things_1)
+            changelist_1 = []
+
 if searchmode == "2":
-    chastfilter = int(input("С какой минимальной частотой будут встречаться значимые имена персонажей? "))
+    chastfilter = int(input("С какой минимальной частотой будут встречаться значимые имена персонажей? "))   
+
 file = open(filename, "r", encoding = "utf-8")
 writefile = open("log.txt", "w")
-writecsv = open(filename.split(".")[0] + "_graph.csv", "w")
-writefile.write(filename + "\n" + str(chastfilter) + "\n" + abzinfo + "\n")
-nlp = spacy.load("en_core_web_sm")
-dict1 = {} #словарь с номерами абзацев
-dict2 = {} #словарь частотности
+
+if searchmode == "1" or searchmode == "2":
+    writecsv = open(filename.split(".")[0] + "_graph.csv", "w")
+    writefile.write(filename + "\n" + abzinfo + "\n")
+
+if searchmode == "2" or searchmode == "3":
+    nlp = spacy.load("en_core_web_sm")
+
+dict1 = {} #словарь с номерами кусков текста
+dict2 = {} #словарь для частотности
 dict3 = {} #вспомогательный словарь для сортировки charlist по частотности
-pairdict = {}
+pairdict = {} # словарь для вывода режима 2
 charlist = [] #список персонажей
-finallist = [] #charlist но по частотности2
+finallist = [] #charlist но по частотности
+finallist_chast = []
+finaldict = {} # то же, что finallist, но для режима 3
+chapterlist = [] # для режима 3: сюда пока идут словари с частотностью имен по главам
 count = 0
 check = 0
 text = file.read()
-if abzinfo == "1":
-    textlist = text.split("\n\n")
-if abzinfo == "2":
-    textlist = text.split("\n")
-if abzinfo == "3":
-    textlist = text.split("\t")
+
+textlist = text.split(abzinfo)
 for i in tqdm(range(len(textlist))):
     count += 1 #счетчик номеров абзаца для словаря ниже
-    abz = textlist[i].strip() #чистим
-    if searchmode == "2":
-        res1 = normnamefinder(abz) #ищем через спейси все то, что похоже на имя, теперь надо убрать оттуда все, что с маленькой буквы
-        for elem in res1.copy(): # убираем из выдачи спейси все, что с маленькой буквы, итерируемся по копии списка во избежание
+    abz = textlist[i].strip() #чистим текст
+    if searchmode == "2" or searchmode == "3":
+        res = normnamefinder(abz) #ищем через спейси все то, что похоже на имя
+        for elem in res.copy(): #убираем из выдачи спейси все, что с маленькой буквы, итерируемся по копии списка во избежание
             if elem.islower() == True:
-                res1.remove(elem)
-        dict1[count] = res1 # записываем в словарь под номером абзаца
-    if searchmode == "1":
-        res1 = []
-        for ima in worklist_us:
+                res.remove(elem)
+        dict1[count] = res # записываем в словарь под номером абзаца
+        if searchmode == "3": # для режима 3: собираем список со словарями частотности для каждого куска текста
+            for wgs in res:
+                if wgs not in dict3:
+                    dict3[wgs] = 0
+                if wgs in dict2:
+                    dict3[wgs] += 1
+            chapterlist.append(dict3)
+            dict3 = {}
+    if searchmode == "1": # для режима 1: забиваем на спейси и просто присоединяем те имена из worklist_1, которые есть в абзаце. Довольно лениво.
+        res = []
+        for ima in worklist_1:
             if ima in abz:
-                res1.append(ima)
-            dict1[count] = res1
-            
-if searchmode == "1":
-    for pair_us in dict1.items():
-        for nme in pair_us[1].copy():
-            for clist_us in changebiglist_us:
-                if nme in clist_us:
-                    pair_us[1].remove(nme)
-                    if clist_us[0] not in pair_us[1]:
-                        pair_us[1].append(clist_us[0])
+                res.append(ima)
+            dict1[count] = res
 
-for wtuki in dict1: # собираем словарь частотности
-    for k in range (len(dict1[wtuki])):
-        if dict1[wtuki][k] not in dict2:
-            dict2[dict1[wtuki][k]] = 0
-        if dict1[wtuki][k] in dict2:
-            dict2[dict1[wtuki][k]] = dict2[dict1[wtuki][k]] + 1
+if searchmode == "3": # для режима 3: для каждого словаря из chapterlist ищем топ-3 самых частотных имени собственных. 
+    for dh in range(len(chapterlist)):
+        if len(chapterlist[dh]) > 3:
+            for s in range (3):
+                for keys in chapterlist[dh]:
+                    if chapterlist[dh][keys] >= check:
+                        check = chapterlist[dh][keys]
+                        mean = keys
+                finallist_chast.append(mean)
+                check = 0
+                chapterlist[dh].pop(mean)
+            finaldict[dh] = finallist_chast
+            finallist_chast = []
+        if len(chapterlist[dh]) <= 3:
+            for elems in chapterlist[dh]:
+                finallist_chast.append(elems)
+            finaldict[dh] = finallist_chast
+            finallist_chast = []
+    print(finaldict)
 
-writefile.write(str(dict1) + "\n\n") # с номерами абзацев
-writefile.write(str(dict2) + "\n\n") # с частотностью
+if searchmode == "1": # 
+    for pair_1 in dict1.items():
+        for name_1 in pair_1[1].copy():
+            for clist_1 in changebiglist_1:
+                if name_1 in clist_1:
+                    pair_1[1].remove(name_1)
+                    if clist_1[0] not in pair_1[1]:
+                        pair_1[1].append(clist_1[0])
 
-for things in dict1:
-    for w in range (len(dict1[things])): #записываем в charlist уникальные вхождения имен героев, потом отдадим это юзеру
-        if str(dict1[things][w]) not in charlist and dict2[dict1[things][w]] > chastfilter: #фильтр для отсечения возможных опечаток.
-            charlist.append(dict1[things][w])
-
-for es in charlist: #формируем список по убыванию частотности
-    dict3[es] = dict2[es]
-for l in range (len(charlist)):
-    for key in dict3:
-        if dict3[key] >= check:
-            check = dict3[key]
-            mean = key
-    finallist.append(mean)
-    check = 0
-    dict3.pop(mean)
-    
-writefile.write("Список частотности: " + str(finallist))    
 if searchmode == "2":
+    for wtuki in dict1: # собираем словарь частотности
+        for k in range (len(dict1[wtuki])):
+            if dict1[wtuki][k] not in dict2:
+                dict2[dict1[wtuki][k]] = 0
+            if dict1[wtuki][k] in dict2:
+                dict2[dict1[wtuki][k]] = dict2[dict1[wtuki][k]] + 1
+    
+    for things in dict1:
+        for w in range (len(dict1[things])): #записываем в charlist уникальные вхождения имен героев, потом отдадим это юзеру
+            if str(dict1[things][w]) not in charlist and dict2[dict1[things][w]] >= chastfilter: #фильтр для отсечения возможных опечаток.
+                charlist.append(dict1[things][w])
+
+    for es in charlist: #формируем список по убыванию частотности
+        dict3[es] = dict2[es]
+    for l in range (len(charlist)):
+        for key in dict3:
+            if dict3[key] >= check:
+                check = dict3[key]
+                mean = key
+        finallist.append(mean)
+        check = 0
+        dict3.pop(mean)
+    
+    writefile.write(str(dict1) + "\n\n") #с номерами абзацев 
+    writefile.write(str(dict2) + "\n\n") # с частотностью
+    writefile.write("Список частотности: " + str(finallist))    
+    
     print("Вот оно всё: ", finallist)
     print("Введите, пожалуйста, имена героев, которые Вас интересуют, разделяя их пробелами. Если разные имена в списке - варианты имени одного персонажа, то введите их через _ Первым впишите то имя, которое хотите видеть в графике")
     readstr = input("")
@@ -135,18 +174,20 @@ if searchmode == "2":
         cleary = set(pair[1])
         clearly = list(cleary)
         dict1[pair[0]] = clearly
-    
-for pair in dict1.items():
-    if len(pair[1]) > 1:
-        for j in combinations(pair[1],2):
-            j = tuple(sorted(j))
-            if j in pairdict:
-                pairdict[j] += 1
-            if j not in pairdict:
-                pairdict[j] = 1
-writecsv.write("Source,Type,Target,Weight" + "\n")
-for enters in pairdict.items():
-    writecsv.write(enters[0][0] + "," + "Undirected" + "," + enters[0][1] + "," + str(enters[1]) + "\n")
+
+if searchmode == "1" or searchmode == "2":
+    for pair in dict1.items():
+        if len(pair[1]) > 1:
+            for j in combinations(pair[1],2):
+                j = tuple(sorted(j))
+                if j in pairdict:
+                    pairdict[j] += 1
+                if j not in pairdict:
+                    pairdict[j] = 1
+    writecsv.write("Source,Type,Target,Weight" + "\n")
+    for enters in pairdict.items():
+        writecsv.write(enters[0][0] + "," + "Undirected" + "," + enters[0][1] + "," + str(enters[1]) + "\n")
+    writecsv.close()
+
 file.close()
 writefile.close()
-writecsv.close()
